8d1fa381c7a530de7716db0b6f9a3804731e812b
Alex49er <151443937+Alex49er@users.noreply.github.com>
2024-08-05 18:35:25 +0100

    added styles and responsivness to the possibility component

--- src/containers/possibility/possibility.jsx
+++ src/containers/possibility/possibility.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './possibility.css'
+import possiblityImage from '../../assets/possibility.png';
 
 const Possibility = () => {
   return (
-    <div>
-      Possibility
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possiblityImage} alt='possibility' />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>
+          Request early access to get started
+        </h4>
+        <h1 className='gradient__text'>
+          The Possibilities Are Beyond Our Imagination.
+        </h1>
+        <p>Yet tavelling bed for any assistance is vital for the number institue redact.</p>
+        <h4>Request Early Access To Get Started</h4>
+
+      </div>
     </div>
   )
 }
